3cf7d11545f9f22f8ab71f8987a6115f96f91e1a
anisda23-meet <anisdai690@gmail.com>
2022-08-02 19:31:41 +0300

    completed

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -15,6 +15,7 @@ config = {
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
+db = firebase.database()
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
@@ -37,11 +38,16 @@ def signin():
 def signup():
     if request.method == 'POST':
         email = request.form['email']
+        full_name = request.form['full_name']
+        username = request.form['username']
+        bio = request.form['bio']
         password = request.form['password']
+        user = {"email" : email, "name": full_name, "username" : username, "bio" : bio, "password" : password}
         try:
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            db.child('Users').child(login_session['user']['localId']).set(user)
         except:
-           error = "Authentication failed"
+           error = "operation failed"
         return redirect(url_for('add_tweet'))
     else:
         return render_template("signup.html")
@@ -49,7 +55,22 @@ def signup():
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['text']
+        uid= db.child('Users').child(login_session['user']['localId']).get().val()
+        tweet = {"title" : title, "text" : text, "uid" : uid}
+        try:
+            db.child('Tweets').push(tweet)
+        except:
+            error = "operation failed"
     return render_template("add_tweet.html")
+
+@app.route('/tweets', methods=['GET', 'POST'])
+def tweets():
+
+    return render_template("tweets.html", tweets = db.child('Tweets').get().val())
+
 
 
 if __name__ == '__main__':
